e258c119bc969fb9eb016612244f5210e9b4a2e1
gabehug <gaberhug@gmail.com>
2025-05-10 17:37:46 -0500

    initial lazy setup and deletion of most packer files

--- lua/ghug/lazy.lua
+++ lua/ghug/lazy.lua
@@ -29,9 +29,4 @@ require("lazy").setup({
 		-- import your plugins
 		{ import = "ghug/plugins" },
 	},
-	-- Configure any other settings here. See the documentation for more details.
-	-- colorscheme that will be used when installing plugins.
-	--install = { colorscheme = { "habamax" } },
-	-- automatically check for plugin updates
-	--checker = { enabled = true },
 })
--- lua/ghug/plugins/treesitter.lua
+++ lua/ghug/plugins/treesitter.lua
@@ -1,15 +1,16 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",  -- Set up the build command
+  config = function()
+    local configs = require("nvim-treesitter.configs")
 
-require("lazy").setup({{
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function () 
-      local configs = require("nvim-treesitter.configs")
+    -- Configure Treesitter settings
+    configs.setup({
+      ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
+      sync_install = false,
+      highlight = { enable = true },
+      indent = { enable = true },
+    })
+  end
+}
 
-      configs.setup({
-          ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
-          sync_install = false,
-          highlight = { enable = true },
-          indent = { enable = true },  
-        })
-    end
- }})
