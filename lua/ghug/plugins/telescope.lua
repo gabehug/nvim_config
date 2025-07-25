253440896f6a6742c86bf21b1839966d6365dda3
gabehug <gaberhug@gmail.com>
2025-07-25 08:34:20 -0500

    adds lualine, markdownpreview, rendermarkdown, oil, and makes some changes to setup

--- lua/ghug/plugins/telescope.lua
+++ lua/ghug/plugins/telescope.lua
@@ -3,6 +3,17 @@ return {
     'nvim-telescope/telescope.nvim',
     dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
+        require("telescope").setup({
+            defaults = {
+                layout_strategy = "vertical",
+                layout_config = {
+                    vertical = {
+                        width = 300,
+                        height = 100,
+                    },
+                },
+            },
+        })
         local builtin = require('telescope.builtin')
 
         vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
